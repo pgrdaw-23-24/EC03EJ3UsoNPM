d2b05fccd8a3a73b269552652778932f47bc2aee
pgrdaw <pgrdaw@gmail.com>
2023-11-07 22:54:55 +0100

    subiendo carpeta al repositorio

---
+++ node_modules/@pgrdaw/bananize/bananize.js
@@ -0,0 +1,9 @@
+function banana(array) {
+    let banana = []
+    for (elemento of array) {
+        banana.push('banana')
+    }
+    return banana
+}
+
+module.exports = banana
---
+++ test.js
@@ -0,0 +1,3 @@
+const banana = require("@pgrdaw/bananize")
+
+console.log (banana([8, true, 'dsfg', null,  [8, 'sdf'], 8]))
